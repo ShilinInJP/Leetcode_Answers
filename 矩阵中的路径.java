013da723b17cde1182c998a7763a58b47b22214b
ShilinInJP <72908049+ShilinInJP@users.noreply.github.com>
2021-01-07 18:36:38 +0900

    Update 矩阵中的路径.java

--- 矩阵中的路径.java
+++ 矩阵中的路径.java
@@ -23,3 +23,24 @@ class Solution {
 链接：https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/solution/mian-shi-ti-12-ju-zhen-zhong-de-lu-jing-shen-du-yo/
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+    public boolean exist(char[][] board, String word) {
+        for(int i=0; i<board.length;i++){
+            for(int j=0; j<board[0].length;j++){
+                if(dfs(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    boolean dfs(char[][] board, String word, int i, int j,int k){
+        char index;
+        index = word.charAt(k);
+        if(i>=board.length|| i<0 || j>=board[0].length || j<0 || index != board[i][j] ) return false;
+        if(k == word.length()-1) return true;
+        board[i][j] = '\0';
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) || 
+                      dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i , j - 1, k + 1);
+        board[i][j] = index;
+        return res;
+    }
